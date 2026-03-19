453567b7148f4e18f07de83179b8a608daebb1a4
LumineerCo <lumineerco@yahoo.com>
2026-03-20 00:05:24 +0530

    Create Header component

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <h1>LumineerCo Service Provider</h1>
+        </header>
+    );
+};
+
+export default Header;
